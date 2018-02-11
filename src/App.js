e39138863bce776074f5ac4df4eef03d7c703534
Sinan Aksay <me@wunnle.com>
2018-02-11 13:26:55 +0300

    merging with blog repo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './style.css';
+
+class Article extends Component {
+  render() {
+    return(
+      <article>
+      <h2><i className="emoji">{this.props.emoji}</i><a href="#">{this.props.title}</a></h2>
+        <div className="details">
+            <a href="#">{this.props.cat}</a>
+            <a href="#">3 minute read</a>
+        </div>
+        {this.props.children}
+    </article>
+    )
+  }
+}
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      movies: [],
-      dataURL: 'http://wunnle.com/headless/wp-json/wp/v2/movies?_embed'
+      articles: [],
+      dataURL: 'http://wunnle.com/headless/wp-json/wp/v2/article?_embed'
     }
   }
 
@@ -17,19 +32,37 @@ class App extends Component {
       .then(res => res.json())
       .then(res => {
         this.setState({
-          movies: res
+          articles: res
         })
       })
   }
 
   render() {
-    console.log('movies', this.state.movies)
+    console.log('articles', this.state.articles)
     return (
       <div className="App">
-        <h2>Cool Space Movies</h2>
-        <p>{this.state.movies.map(movie => {
-          return <div>{movie.title.rendered}</div>
-        })}</p>
+          <div className="content">
+        <div className="container">
+            <header>
+                <i className="papership"></i>
+                <h1>A web design<a href="index.html">blog</a></h1>
+            </header>
+            <p>
+            {this.state.articles.map(article => 
+            <Article key="{article.key}" title={article.title.rendered} cat={article.acf.category} emoji={article.acf.emoji}>
+              As a designer, your job is to understand your client’s needs. Listening to what they tell you is a good place to start, but it doesn’t end there. You gain much more insight by asking the right questions. Of course, it also helps to ask the right people and ask in the right way. Janice Gervais offers some tips to turn you into a better designer/detective.
+            </Article>
+            )}</p>
+            <div className="pagination">
+                <a className="right" href="#">See older articles</a>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div className="container">
+            <i className="papership-bw"></i>
+        </div>
+    </footer>
       </div>
     );
   }
