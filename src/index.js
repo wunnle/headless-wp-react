e5dede2fd45d694e19068e167db795a0c83eb065
Sinan Aksay <me@wunnle.com>
2018-02-25 17:58:51 +0300

    react router scroll fix

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import ScrollToTop from "./Components/ScrollToTop";
 import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter onUpdate={() => window.scrollTo(0, 0)}>
-    <App />
+  <BrowserRouter>
+    <ScrollToTop>
+      <App />
+    </ScrollToTop>
   </BrowserRouter>,
   document.getElementById("root")
 );
